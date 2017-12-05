35d123dcaf47f4e7c3c9c720747987dfb1335c80
Blair Beckwith <hi@blair.lol>
2017-12-05 10:23:03 -0500

    Fixed hello typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "h?e llo, world!"
+    render html: "hello, world!"
   end
 end
